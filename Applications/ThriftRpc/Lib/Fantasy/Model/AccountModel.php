bd54fad5658ecc99b8b1944e29ce1f184d88af88
3032441712 <3032441712@qq.com>
2015-06-12 23:39:27 +0800

    TODO 1 添加账号处理模块与处理服务 2 添加日志记录服务.

---
+++ Applications/ThriftRpc/Lib/Fantasy/Model/AccountModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fantasy\Model;
+
+class AccountModel
+{
+    public static function fantasyAccountAdd()
+    {
+        
+    }
+
+    public static function fantasyAccountUpdate()
+    {
+        
+    }
+
+    public static function fantasyAccountInfo()
+    {
+        
+    }
+
+    public static function fantasyAccountList()
+    {
+        
+    }
+}
