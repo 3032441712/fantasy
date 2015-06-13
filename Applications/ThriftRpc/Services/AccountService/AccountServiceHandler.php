c97262bedb7eb65278b7b5a361041da8cb969e1c
3032441712 <zhaoyan@hichao.com>
2015-06-13 15:29:59 +0800

    TODO 1 修正错误异常的提示方式.

--- Applications/ThriftRpc/Services/AccountService/AccountServiceHandler.php
+++ Applications/ThriftRpc/Services/AccountService/AccountServiceHandler.php
@@ -20,21 +20,39 @@ class AccountServiceHandler extends ThriftService implements AccountServiceIf
 
     public function fantasyAccountUpdate($input_data)
     {
-        $app_params = $this->loadParams($input_data);
+        try {
+            $app_params = $this->loadParams($input_data);
+        } catch (\PDOException $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => '数据库级别错误,请通知接口负责人.']);
+        } catch (\Exception $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => $e->getMessage()]);
+        }
 
         return $this->response(0, 'success', '账户更新成功');
     }
 
     public function fantasyAccountInfo($input_data)
     {
-        $app_params = $this->loadParams($input_data);
+        try {
+            $app_params = $this->loadParams($input_data);
+        } catch (\PDOException $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => '数据库级别错误,请通知接口负责人.']);
+        } catch (\Exception $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => $e->getMessage()]);
+        }
 
         return $this->response(0, 'success', '查询账号详细信息');
     }
 
     public function fantasyAccountList($input_data)
     {
-        $app_params = $this->loadParams($input_data);
+        try {
+            $app_params = $this->loadParams($input_data);
+        } catch (\PDOException $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => '数据库级别错误,请通知接口负责人.']);
+        } catch (\Exception $e) {
+            throw new Apiception(['code' => $e->getCode(), 'message' => $e->getMessage()]);
+        }
 
         return $this->response(0, 'success', '账号列表');
     }
